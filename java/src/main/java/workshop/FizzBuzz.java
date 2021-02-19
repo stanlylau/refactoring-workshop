66b7d1b2497a3645ce2c407e4359779465105bdd
Stanly Lau <stanly@odd-e.com>
2021-02-19 15:00:12 +0800

    Fizzbuzz requirement

--- java/src/main/java/workshop/FizzBuzz.java
+++ java/src/main/java/workshop/FizzBuzz.java
@@ -1,5 +1,11 @@
 package workshop;
 
+/**
+ * Requirements:
+ * For factor of three print Fizz instead of the number
+ * For factor of five print Buzz instead of the number
+ * For numbers which are factors of both three and five print FizzBuzz instead of the number
+ */
 public class FizzBuzz {
     public static String say(int number) {
         String strReturn = null;
